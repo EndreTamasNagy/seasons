4f3d38256fdd8f0bed1ea8cb79968177ef9594c6
Endre Nagy <endre.nagy@evosoft.com>
2019-03-29 13:37:08 +0100

    after 6-10

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -26,10 +26,10 @@ const getSeason = (latitude, month) => {
 const SeasonDisplay = (props) => {
     const season = getSeason(props.latitude, new Date().getMonth());
     
-    const {text, iconName } = seasonConfig[season] // {text, iconName}
+    const {text, iconName } = seasonConfig[season]; // {text, iconName}
 
     return ( 
-        <div>
+        <div className={`season-display ${season}`}>
             <i className={`icon-left massive ${iconName} icon`} />
             <h1>{text}</h1>
             <i className={`icon-right massive ${iconName} icon`} />
